f50e5a520918b0819ebcc04f5a6bda962bd62f11
tomz123 <tomz12321@gmail.com>
2018-09-04 01:14:53 +1000

    Init Project folder SinglyLinkedList
    Init Project folder SinglyLinkedList
    - for DS and AA learning

---
+++ LinkedList.java
@@ -0,0 +1,171 @@
+
+/**
+ * Write a description of class LinkedList here.
+ * Linked List | Set 2 (Inserting a node)
+ *
+ * @author Jyh-woei Yang
+ * @version 2018/09/04
+ */
+public class LinkedList
+{
+    // instance variables - replace the example below with your own
+    Node head; //head of list
+
+    /**
+     * Constructor for objects of class LinkedList
+     */
+    public LinkedList()
+    {
+        // initialise instance variables
+        //x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return 0;
+        // return your value here
+    }
+    
+    /**
+     * Node Class
+     */
+    class Node{
+        int data;
+        Node next;
+        
+        // Constructor to create a new node
+        Node(int d) 
+        {
+            data = d;
+            next = null;
+        }
+    }
+    
+    /**
+     * This function is in LinkedList class. 
+     * Inserts a new Node at front of the list. 
+     * This method is defined inside LinkedList class shown above
+     */
+    public void push(int new_data)
+    {
+        /* 1 & 2: Allocate the Node & Put in the data*/
+        Node new_node = new Node(new_data);
+ 
+        /* 3. Make next of new Node as head */
+        new_node.next = head;
+ 
+        /* 4. Move the head to point to new Node */
+        head = new_node;
+    }
+    
+    /**
+     * This function is in LinkedList class.
+     * Inserts a new node after the given prev_node. 
+     * This method is defined inside LinkedList class shown above 
+     */
+    public void insertAfter(Node prev_node, int new_data)
+    {
+        /* 1. Check if the given Node is null */
+        if (prev_node == null)
+        {
+            System.out.println("The given previous node cannot be null");
+            return;
+        }
+     
+        /* 2. Allocate the Node &
+           3. Put in the data*/
+        Node new_node = new Node(new_data);
+     
+        /* 4. Make next of new Node as next of prev_node */
+        new_node.next = prev_node.next;
+     
+        /* 5. make next of prev_node as new_node */
+        prev_node.next = new_node;
+    }
+    
+    /** 
+     * Appends a new node at the end.  
+     * This method is defined inside LinkedList class shown above 
+     */
+    public void append(int new_data)
+    {
+        /* 1. Allocate the Node &
+           2. Put in the data
+           3. Set next as null */
+        Node new_node = new Node(new_data);
+     
+        /* 4. If the Linked List is empty, then make the
+               new node as head */
+        if (head == null)
+        {
+            head = new Node(new_data);
+            return;
+        }
+     
+        /* 4. This new node is going to be the last node, so
+             make next of it as null */
+        new_node.next = null;
+     
+        /* 5. Else traverse till the last node */
+        Node last = head; 
+        while (last.next != null)
+            last = last.next;
+     
+        /* 6. Change the next of last node */
+        last.next = new_node;
+        return;
+    }
+    
+    /** 
+     * This function prints contents of linked list starting from the given node 
+     */
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+" ");
+            tnode = tnode.next;
+        }
+    }
+ 
+    /** 
+     * Driver program to test above functions. 
+     * Ideally this function should be in a separate user class.  
+     * It is kept here to keep code compact. 
+     */
+    public static void main(String[] args)
+    {
+        /* Start with the empty list */
+        LinkedList llist = new LinkedList();
+ 
+        // Insert 6.  So linked list becomes 6->NUllist
+        llist.append(6);
+ 
+        // Insert 7 at the beginning. So linked list becomes
+        // 7->6->NUllist
+        llist.push(7);
+ 
+        // Insert 1 at the beginning. So linked list becomes
+        // 1->7->6->NUllist
+        llist.push(1);
+ 
+        // Insert 4 at the end. So linked list becomes
+        // 1->7->6->4->NUllist
+        llist.append(4);
+ 
+        // Insert 8, after 7. So linked list becomes
+        // 1->7->8->6->4->NUllist
+        llist.insertAfter(llist.head.next, 8);
+ 
+        System.out.println("\nCreated Linked list is: ");
+        llist.printList();
+    }
+}
